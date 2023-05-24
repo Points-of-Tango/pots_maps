b009881af5da7f5e65bd51a7861f2be73062e16a
Daniel Gini <daniel.gini@gmail.com>
2023-05-24 18:09:01 +0100

    build/deploy fixed

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,12 @@
 #mv dist/css/ public/
 #mv dist/img/ public/
 #mv dist/js/ public/
-#mv dist/favicon.ico public/
 #mv dist/index.html public/
-#mv dist/manifest.json public/
 #mv dist/404.html public/
+#mv dist/manifest.json public/
 #mv dist/workbox-* public/
 #mv dist/service-worker* public/
 
-cp favicon.ico dist/
+#cp favicon.ico dist/
 
 firebase deploy
